4648b713f6f90e266de5b8523ae0fc55c91589d5
Thanh Cong Chien <thnhcngchin@gmail.com>
2020-11-27 13:51:00 +0700

    Updates

---
+++ precache-manifest.d479beff5a41126a9392a12f1e1e768c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384dbdbf595d100bc8af2e279432c65d",
+    "url": "/Instagram-clone/index.html"
+  },
+  {
+    "revision": "42c03ba52ce4a5d2d9cb",
+    "url": "/Instagram-clone/static/css/main.916fa37e.chunk.css"
+  },
+  {
+    "revision": "43a1c66b23abf0fcc5b7",
+    "url": "/Instagram-clone/static/js/2.873b9579.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Instagram-clone/static/js/2.873b9579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c03ba52ce4a5d2d9cb",
+    "url": "/Instagram-clone/static/js/main.bbe31838.chunk.js"
+  },
+  {
+    "revision": "c6eaa06d0f7bba0d2490",
+    "url": "/Instagram-clone/static/js/runtime-main.2d015c95.js"
+  }
+]);
